2afd17414e93a71dbac7053c412859f04f86b3a3
Vavency <vavency@gmail.com>
2017-11-07 18:04:49 +0200

    Functionality... What?

--- git/vavency/Song.java
+++ git/vavency/Song.java
@@ -3,19 +3,24 @@ package git.vavency;
 import java.util.List;
 
 public class Song {
+
+    private String sng;
+    private List lyr;
+
     public Song(String title, List<String> lyrics)
     {
-
+        this.lyr = lyrics;
+        this.sng = title;
     }
-/*
+
     public List<String> getLyrics()
     {
-
+        return this.lyr;
     }
 
     public String getTitle()
     {
-
+        return this.sng;
     }
-    */
+
 }
--- git/vavency/SongPlayer.java
+++ git/vavency/SongPlayer.java
@@ -1,13 +1,20 @@
 package git.vavency;
 
+import java.util.List;
+
 public class SongPlayer
 {
+    private int spd;
+    private Song sng = new Song(null, null);
+
     public void setSpeed(int speed)
     {
+        this.spd = speed;
     }
 
     public void setSong(final Song song)
     {
+
     }
 
     public void play()
